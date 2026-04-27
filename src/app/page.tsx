04c1982f8eeb3c542940f784a0a00e457eb2fe82
Misael <misaelborges1981@gmail.com>
2026-04-27 18:01:54 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import { db } from "@rangooo/database"
+import { prisma } from "@misael1981/rangooo-database"
 
 export default async function Home() {
-  const restaurants = await db.restaurant.findMany({
+  const restaurants = await prisma.restaurant.findMany({
     take: 1,
   })
-  console.log(restaurants)
 
   return (
     <div>
-      <h1>Testando</h1>
+      <h1>{restaurants[0].name}</h1>
     </div>
   )
 }
