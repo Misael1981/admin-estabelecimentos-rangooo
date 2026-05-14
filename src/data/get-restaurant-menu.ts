4b91ce17da56d2048b59e0a2996da1dcedc4f82f
Misael <misaelborges1981@gmail.com>
2026-05-14 19:49:00 -0300

    Correção do map do menuCategories

--- src/data/get-restaurant-menu.ts
+++ src/data/get-restaurant-menu.ts
@@ -1,6 +1,26 @@
-import { prisma } from "@misael1981/rangooo-database"
+import { Prisma, prisma } from "@misael1981/rangooo-database"
 
-export const getRestaurantMenuBySlug = async (slug: string) => {
+type RestaurantMenuCategory = Prisma.MenuCategoryGetPayload<{
+  include: {
+    _count: { select: { products: true } }
+    products: {
+      select: {
+        id: true
+        name: true
+        price: true
+        imageUrl: true
+        description: true
+        ingredients: true
+        isVisible: true
+      }
+    }
+    additionalIngredients: true
+  }
+}>
+
+export const getRestaurantMenuBySlug = async (
+  slug: string,
+): Promise<RestaurantMenuCategory[]> => {
   return await prisma.menuCategory.findMany({
     where: {
       restaurant: { slug },
