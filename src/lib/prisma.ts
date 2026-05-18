e7354b11eb982f386637f0cd480780081f0694d6
Misael <misaelborges1981@gmail.com>
2026-05-17 22:55:21 -0300

    Teste prisma/client-3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension"
+import { PrismaClient } from "@misael1981/rangooo-database"
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
